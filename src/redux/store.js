72e2abb7c3c568ca0e3911265d5adfddb4072bea
Vitalii Kozha <116120008+KozhaVitalii@users.noreply.github.com>
2023-08-24 19:15:23 +0300

    added_all_tasks

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { persistedPhoneBook } from './phonebook/slice';
+
+export const store = configureStore({
+  reducer: {
+    phoneBook: persistedPhoneBook,
+  },
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
